c35588a11a5f8f451e933e06af7ef9c873291f26
nobuhiroK <nobuhk@uw.edu>
2018-11-18 09:11:34 +0900

    React Native Components and Navigation

---
+++ components/ContactComponent.js
@@ -0,0 +1,44 @@
+import { Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+import React, { Component } from 'react';
+
+function RenderContact() {
+
+            return(
+                <Card title='Contact Information'>
+                    <Text >
+                        121, Clear Water Bay Road {"\n"}
+                    </Text>
+                    <Text >
+                        Clear Water Bay, Kowloon {"\n"}
+                    </Text>
+                    <Text >
+                        HONG KONG {"\n"}
+                    </Text>
+                    <Text >
+                        Tel: +852 1234 5678 {"\n"}
+                    </Text>
+                    <Text >
+                        Fax: +852 8765 4321 {"\n"}
+                    </Text>
+                    <Text>
+                        Email:confusion@food.net
+                    </Text>
+                </Card>
+            );
+        }
+class Contact extends Component {
+
+   
+    static navigationOptions = {
+        title: 'Contact Us'
+    };
+
+    render() {
+        return(
+            <RenderContact  />
+        );
+    }
+}
+
+export default Contact;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -39,6 +41,32 @@ const HomeNavigator = createStackNavigator({
       headerTintColor: "#fff"  
     })
 });
+const ContactNavigator = createStackNavigator({
+    Home: { screen: Contact }
+  }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff"  
+    })
+});
+const AboutNavigator = createStackNavigator({
+    Home: { screen: About }
+  }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff"  
+    })
+});
 const MainNavigator = createDrawerNavigator({
     Home: 
       { screen: HomeNavigator,
@@ -47,13 +75,28 @@ const MainNavigator = createDrawerNavigator({
           drawerLabel: 'Home'
         }
       },
+    About: 
+      { screen: AboutNavigator,
+        navigationOptions: {
+          title: 'About Us',
+          drawerLabel: 'About Us'
+        }
+      },
     Menu: 
       { screen: MenuNavigator,
         navigationOptions: {
           title: 'Menu',
           drawerLabel: 'Menu'
+        }
+      },
+    Contact: 
+      { screen: ContactNavigator,
+        navigationOptions: {
+          title: 'Contact Us',
+          drawerLabel: 'Contact Us'
         }, 
       }
+
 }, {
   drawerBackgroundColor: '#D1C4E9'
 });
